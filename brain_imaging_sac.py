1ce82dd8099a2dab3b91cbe0220d6934a4790b41
Zman94 <zgleason94@gmail.com>
2017-11-30 16:55:02 -0600

    Pushing to ews

--- brain_imaging_sac.py
+++ brain_imaging_sac.py
@@ -1,20 +1,29 @@
+'''
+'
+'''
 import time
 # import pdb
-import sys
+# import sys
 import numpy as np
 from sklearn.preprocessing import StandardScaler
 from sklearn.neural_network import MLPClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+from sklearn.metrics import classification_report, accuracy_score, roc_auc_score
 
 def load_data():
+    '''
+    '
+    '''
     tag_name = np.load('./CS446-project/tag_name.npy')
-    train_Y  = np.load('./CS446-project/train_binary_Y.npy')
-    train_X  = np.load('./CS446-project/train_X.npy')
-    test_X   = np.load('./CS446-project/valid_test_X.npy')
+    train_Y = np.load('./CS446-project/train_binary_Y.npy')
+    train_X = np.load('./CS446-project/train_X.npy')
+    test_X = np.load('./CS446-project/valid_test_X.npy')
     return tag_name, train_Y, train_X, test_X
 
 def manual_reshape(train_X):
+    '''
+    '
+    '''
     train_X_2d = []
     for i in range(len(train_X)):
         train_X_2d.append([])
@@ -50,7 +59,7 @@ def main():
     train_X_2d = scaler.transform(train_X_2d)
 
     ### Train/Test Split ###
-    x_train, x_test, y_train, y_test = train_test_split(train_X_2d, train_Y, test_size = .25)
+    x_train, x_test, y_train, y_test = train_test_split(train_X_2d[:2000], train_Y[:2000], test_size = .25)
 
     ### Train Classifier ###
     # vanilla_nn(train_X_2d, train_Y, test_X)
@@ -58,21 +67,23 @@ def main():
     ### Train Classifier (train/test) ###
     predictions = []
     for i in range(len(y_train[0])):
-        mlp = MLPClassifier()
+        mlp = MLPClassifier(hidden_layer_sizes=(1000, 50))
         mlp.fit(x_train, y_train[:, i])
         predictions.append(mlp.predict(x_test))
         print("----", tag_name[i] + ",", i, "----")
         print(accuracy_score(y_test[:, i], predictions[i]))
+        print(roc_auc_score(y_test[:, i], predictions[i]))
 
     np_predict = np.column_stack((np.array(i) for i in predictions))
     print(accuracy_score(y_test, np_predict))
+    print(roc_auc_score(y_test, np_predict))
 
     # print(classification_report(y_test, predictions))
     # print(predictions)
 
 if __name__ == "__main__":
     # pdb.set_trace()
-    start_time = time.time()
+    STARTTIME = time.time()
     main()
     print("--- {0} minutes {1} seconds ---".format( \
-       int((time.time() - start_time)/60), int((time.time() - start_time)%60)))
+       int((time.time() - STARTTIME)/60), int((time.time() - STARTTIME)%60)))
